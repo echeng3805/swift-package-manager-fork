35839175eee204745cb23790cd3c751a46470b07
Ev Cheng <ev_cheng@apple.com>
2026-02-20 09:53:26 -0800

    refactor: specs

--- Sources/SBOMModel/Encoder/SBOMEncoder.swift
+++ Sources/SBOMModel/Encoder/SBOMEncoder.swift
@@ -27,9 +27,9 @@ internal struct SBOMEncoder {
         if !fileSystem.exists(outputDir) {
             try fileSystem.createDirectory(outputDir, recursive: true)
         }
-        let specs = await Self.getSpecs(from: specs)
+        let sbomSpecs = Array(Set(specs.map { $0.internalSpec() })).sorted()
         var results: [SBOMResult] = []
-        for spec in specs {
+        for spec in sbomSpecs {
             let outputPath = try await self.encodeSBOM(spec: spec, outputDir: outputDir, filter: filter, fileSystem: fileSystem)
             results.append(SBOMResult(spec: spec, path: outputPath))
         }
@@ -65,18 +65,6 @@ internal struct SBOMEncoder {
         try await self.validateSBOM(from: encoded, spec: spec)
 
         return encoded
-    }
-
-    internal static func getSpec(from spec: Spec) async -> SBOMSpec {
-        return SBOMSpec(spec: spec)
-    }
-
-    internal static func getSpecs(from specs: [Spec]) async -> [SBOMSpec] {
-        var result = Set<SBOMSpec>()
-        for spec in specs {
-            await result.insert(self.getSpec(from: spec))
-        }
-        return Array(result).sorted()
     }
 
     internal func validateSBOM(from encoded: Foundation.Data, spec: SBOMSpec, bundleName: String = "SwiftPM_SBOMModel") async throws {
